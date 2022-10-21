58a90d6eb8369bb924a4a882ebcdc6b23cacd16b
JohnGastone <johngastone11@gmail.com>
2022-10-21 17:44:30 +0300

    Added cards categoryh

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/John.jpg'
+import {BsAward} from 'react-icons/bs'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderActive} from 'react-icons/vsc'
 
 function About() {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+            <div className="about__me-image">
+              <img src={ME} alt='me'/>
+            </div>
+        </div>
+        <div className="about__content">
+            <div className="about__cards">
+              <article className='about__card'>
+                <BsAward className='about__icon'/>
+                  <h5>Experience</h5>
+                  <small>3+ Years Working</small>
+              </article>
+              <article className='about__card'>
+                <FiUsers className='about__icon'/>
+                  <h5>Clients</h5>
+                  <small>100+ Nationalwide</small>
+              </article>              
+              <article className='about__card'>
+                <VscFolderActive className='about__icon'/>
+                  <h5>Projects</h5>
+                  <small>50+ Completed</small>
+              </article>
+    
+            </div>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam nisi laborum esse ipsam repellat recusandae labore alias. 
+                Et aspernatur neque id incidunt, expedita optio architecto aut? Repudiandae qui debitis quasi velit excepturi, doloribus ipsa molestiae quidem ab numquam dolor! 
+                At, voluptatem beatae sed delectus animi quod quo. Vero, libero esse.
+              </p>
+              <a href="#about" className='btn btn-primary'>Let's talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
-import './nav.css'
 import {AiOutlineHome} from 'react-icons/ai'
 import {AiOutlineUser} from 'react-icons/ai'
 import {BsJournalBookmarkFill} from 'react-icons/bs'
 import {RiServiceLine} from 'react-icons//ri'
 import {BiMessage} from 'react-icons/bi'
+import './nav.css'
+import {useState} from 'react' 
 
 function Nav() {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <div>
-      <a href="#"><AiOutlineHome/></a>
-      <a href="#about"><AiOutlineUser/></a>
-      <a href="#experience"><BsJournalBookmarkFill/></a>
-      <a href="#services"><RiServiceLine/></a>
-      <a href="#contact"><BiMessage/></a>
+      <a href="#" className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href="#about" onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
+      <a href="#experience" onClick={()=> setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BsJournalBookmarkFill/></a>
+      <a href="#services" onClick={()=> setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
+      <a href="#contact" onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessage/></a>
     </div>
-  )
+  ) 
 }
 
 export default Nav
