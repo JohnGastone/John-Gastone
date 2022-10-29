f61bb6ee53546626c82352633f8a4d1fb7fd7a01
JohnGastone <johngastone11@gmail.com>
2022-10-29 08:37:22 +0300

    Pushing footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import './footer.css'
+import {FaTelegramPlane} from 'react-icons/fa'
+import {BsInstagram} from 'react-icons/bs'
+import {SlSocialTwitter} from 'react-icons/sl'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className="footer__logo">John Gastone</a>
+      <ul className="permalinks">
+        <li> <a href="#">Home</a> </li>
+        <li> <a href="#about">About</a> </li>
+        <li> <a href="#experience">Experience</a> </li>
+        <li> <a href="#services">Services</a> </li>
+        <li> <a href="#portfolio">Portfolio</a> </li>
+        <li> <a href="#testimonials">Testimonials</a> </li>
+        <li> <a href="#contact">Contact</a> </li>
+      </ul>
+
+      <div className="footer__socials">
+          <a href="https://instagram.com" target='_blank'><BsInstagram/></a>
+          <a href="https://twitter.com" target='_blank'><SlSocialTwitter/></a>
+          <a href="https://telegram.com" target='_blank'><FaTelegramPlane/></a>
+        </div>
+
+        <div className="footer__copyright">
+          <small>&copy; John Gastone. All rights reserved.</small>
+        </div>
+    </footer>
   )
 }
 
