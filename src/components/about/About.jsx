155af3198584df82a97543335154c86461a09593
JohnGastone <johngastone11@gmail.com>
2023-03-17 17:59:48 +0300

    ++

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -27,7 +27,7 @@ function About() {
             <article className='about__card'>
               <FiUsers className='about__icon' />
               <h5>Clients</h5>
-              <small>10+ Nationwide..</small>
+              <small>10+ Locals</small>
             </article>
             <article className='about__card'>
               <VscFolderActive className='about__icon' />
