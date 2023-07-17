0bfa00e28af95e91c0f0cdaad8e1c42d2b7e4045
JohnGastone <johngastone11@gmail.com>
2023-07-17 07:47:15 +0300

    CTA +++

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -6,7 +6,7 @@ function CTA() {
     <div className='cta'>
       <a href={CV} download className='btn'>Download CV</a>
       <a href="#contact" className='btn btn-primary'>Let's talk</a>
-    </div>
+    </div >
   )
 }
 
